a88624e1bd1b814d29711eec728dcfa25613c679
Hamed Bahram <hamed.bahram@gmail.com>
2024-12-09 13:35:43 -0500

    Add serve-side validation

--- components/new-todo-form.tsx
+++ components/new-todo-form.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { useActionState, useState } from 'react'
+import { toast } from 'sonner'
 import { createTodoAction } from '@/lib/actions'
+import { NewTodoSchema } from '@/lib/schemas'
+import { useActionState, useEffect, useState } from 'react'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { NewTodoSchema } from '@/lib/schemas'
 
 export default function NewTodoForm() {
   const [validationError, setValidationError] = useState({ title: '' })
@@ -19,7 +20,6 @@ export default function NewTodoForm() {
     const result = NewTodoSchema.safeParse(data)
 
     if (!result.success) {
-      console.log(result.error.errors[0].message)
       setValidationError({ title: result.error.errors[0].message })
       return
     }
@@ -27,12 +27,41 @@ export default function NewTodoForm() {
     dispatch(formData)
   }
 
+  useEffect(() => {
+    if (state.error) {
+      toast.error(state.error)
+    }
+  }, [state.error])
+
+  useEffect(() => {
+    if (state.success) {
+      toast.success(state.success)
+    }
+  }, [state.success])
+
+  function validate(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target
+    if (name === 'title') {
+      const data = { title: value }
+      const result = NewTodoSchema.safeParse(data)
+      if (result.success) {
+        setValidationError({ title: '' })
+      } else {
+        setValidationError({ title: result.error.errors[0].message })
+      }
+    }
+  }
+
   return (
     <form action={formAction}>
-      <div className='text-red-500'>{state.error}</div>
       <div className='mt-12 flex gap-3'>
         <div className='w-full'>
-          <Input type='text' name='title' placeholder='Enter a task' />
+          <Input
+            type='text'
+            name='title'
+            placeholder='Enter a task'
+            onChange={validate}
+          />
           {validationError.title && (
             <p className='ml-1 mt-2 text-sm text-red-500'>
               {validationError.title}
--- lib/actions.ts
+++ lib/actions.ts
@@ -2,6 +2,7 @@
 
 import { createTodo } from '@/lib/todos'
 import { revalidatePath } from 'next/cache'
+import { NewTodoSchema } from './schemas'
 
 type TodoActionState = {
   success?: string
@@ -12,8 +13,15 @@ export async function createTodoAction(
   state: TodoActionState,
   formData: FormData
 ) {
-  const title = formData.get('title') as string
   try {
+    const data = Object.fromEntries(formData.entries())
+    const result = NewTodoSchema.safeParse(data)
+
+    if (!result.success) {
+      return { error: result.error.errors[0].message }
+    }
+
+    const title = data.title as string
     await createTodo(title)
     return { success: 'Todo added successfully.' }
   } catch (error: unknown) {
